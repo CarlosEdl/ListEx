455c959612cf429ffc64f7bc9b64976ea570c6d0
Carlos Eduardo <97418179+CarlosEdl@users.noreply.github.com>
2023-05-16 19:14:53 -0300

    New Ex

---
+++ LISTA 1/Ex10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salarioAtual, aumento, novoSalario;
+
+            Console.WriteLine("Digite o seu salário");
+            salarioAtual = float.Parse(Console.ReadLine());
+
+            if (salarioAtual < 700) {
+                aumento = salarioAtual * 0.3;
+                novoSalario = salarioAtual + aumento;
+                Console.WriteLine("Seu novo salário é R$ " + novoSalario);
+  
+            }else{
+                aumento = salarioAtual * 0.1;
+                novoSalario = salarioAtual + aumento;
+                Console.WriteLine("Seu novo salário é R$ " + novoSalario);
+
+                
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ LISTA 1/Ex9/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o lado x do triângulo: ");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o lado y do triângulo: ");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o lado z do triângulo: ");
+            double z = double.Parse(Console.ReadLine());
+
+            if (x < y + z && y < x + z && z < x + y)
+            {
+                if (x == y && y == z)
+                {
+                    Console.WriteLine("Triângulo equilátero");
+                }
+                else if (x == y || y == z || x == z)
+                {
+                    Console.WriteLine("Triângulo isósceles");
+                }
+                else
+                {
+                    Console.WriteLine("Triângulo escaleno");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Os lados não correspondem a um triângulo válido.");
+            }
+        }
+    }
+}
