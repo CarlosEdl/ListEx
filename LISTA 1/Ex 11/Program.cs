58babcd669bc19c27f5f486bcfcad92e846d4f6f
Carlos Eduardo <97418179+CarlosEdl@users.noreply.github.com>
2023-05-16 19:38:48 -0300

    Move files

--- LISTA 1/Ex 11/Program.cs
+++ LISTA 1/Ex 11/Program.cs
@@ -10,6 +10,36 @@ namespace Ex_11
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Digite o coeficiente a:");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o coeficiente b:");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o coeficiente c:");
+            double c = double.Parse(Console.ReadLine());
+
+            double delta = (b * b) - (4 * a * c);
+
+            if (delta < 0)
+            {
+                Console.WriteLine("A equação não possui raízes reais.");
+            }
+            else if (delta == 0)
+            {
+                double raiz = -b / (2 * a);
+                Console.WriteLine("A equação possui uma raiz real: " + raiz);
+            }
+            else
+            {
+                double raiz1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double raiz2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine("A equação possui duas raízes reais:");
+                Console.WriteLine("Raiz 1: " + raiz1);
+                Console.WriteLine("Raiz 2: " + raiz2);
+            }
+
+            Console.ReadKey();
         }
     }
 }
